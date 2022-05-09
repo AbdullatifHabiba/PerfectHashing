caa6c31fdabb80ba2cfe9e0f1541dcdabea87672
Abdullatif <ao1228340764a@gmail.com>
2022-05-10 00:35:03 +0200

    TableEntry

--- src/TableEntry.java
+++ src/TableEntry.java
@@ -1,3 +1,33 @@
+import java.util.ArrayList;
+
 public class TableEntry {
+    private ArrayList<Integer> ChainElements;
+    private HTable2 hashT2;
+    public TableEntry() {
+        this.ChainElements = new ArrayList<Integer>();
+        this.hashT2 = new  HTable2();
+    }
+
+
+
+    public ArrayList<Integer> getChainElements() {
+        return ChainElements;
+    }
+
+    public void setChainElements(ArrayList<Integer> chainElements) {
+        ChainElements = chainElements;
+    }
+
+    public HTable2 getHashT2() {
+        return hashT2;
+    }
+
+    public void setHashT2(HTable2 hashT2) {
+        this.hashT2 = hashT2;
+    }
+    public void setKey(int key) {
+        this.ChainElements.add(key);
+    }
+
 
 }
